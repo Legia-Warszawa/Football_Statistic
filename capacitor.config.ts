f662f0264b92cc107969478a5b751f7301821fb6
Dawid <dawidmartyniuk1@gmail.com>
2025-03-10 11:07:41 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.footballstatistic',
+  appName: 'footballstatistic',
+  webDir: 'dist'
+};
+
+export default config;
